885ca5a94b2e2adb17b493d1c03fc5ecbc38f752
MYAyyasy <yahya12ayyasy@gmail.com>
2025-07-04 19:01:25 +0700

    Create process.py

---
+++ api/process.py
@@ -0,0 +1,10 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class Handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Izinkan CORS
+        self.end_headers()
+        self.wfile.write(json.dumps({"success": True, "message": "API bekerja!"}).encode())
