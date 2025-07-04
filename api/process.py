961f283f2517db381374377e312b9de70fef2c86
MYAyyasy <yahya12ayyasy@gmail.com>
2025-07-04 19:19:20 +0700

    Update process.py

--- api/process.py
+++ api/process.py
@@ -5,6 +5,9 @@ class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')  # Izinkan CORS
+        # Izinkan akses dari GitHub Pages dan semua origin (*)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
-        self.wfile.write(json.dumps({"success": True, "message": "API bekerja!"}).encode())
+        self.wfile.write(json.dumps({"success": True, "data": "API bekerja!"}).encode())
